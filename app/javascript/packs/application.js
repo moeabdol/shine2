47806bdc084bcf5e0dd50effa9af179e6c2fceba
moeabdol <mohd.a.saed@gmail.com>
2017-08-01 11:17:27 +0300

    Add bootstrap@3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,5 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./application.css";
 console.log('Hello World from Webpacker');
